7d3e1b03a89298c396ee57004770299beb93ed9e
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-05-01 15:13:47 +0530

    image recogizing AI is completed

--- src/components/objectDetection/index.js
+++ src/components/objectDetection/index.js
@@ -1,6 +1,5 @@
 import "@tensorflow/tfjs-backend-cpu";
 import "@tensorflow/tfjs-backend-webgl";
-// import "@tensorflow/tfjs-core";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 import { useRef, useState } from "react";
 
@@ -14,14 +13,13 @@ const TargetBox = styled.div`
   top: ${({ y }) => y + "px"};
   width: ${({ width }) => width + "px"};
   height: ${({ height }) => height + "px"};
-
-  border: 4px solid #lac71a;
   background-color: transparent;
+  border: 4px solid brown;
   z-index: 20;
 
   &::before {
-    content: "${({ classType, score }) => `${classType} ${score.toFixed(2)}`}";
-    color: #lac71a;
+    content: "${({ classType, score }) => `${classType} ${score.toFixed(1)}%`}";
+    color: brown;
     font-weight: 500;
     font-size: 17px;
     position: absolute;
@@ -32,8 +30,10 @@ const TargetBox = styled.div`
 
 const ObjectDetector = (props) => {
   const fileInputRef = useRef();
+  const imageRef = useRef();
   const [imgData, setImgData] = useState(null);
   const [prediction, setPrediction] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const isEmptyPredications = !prediction || prediction.length === 0;
 
@@ -41,9 +41,33 @@ const ObjectDetector = (props) => {
     if (fileInputRef.current) fileInputRef.current.click();
   };
 
+  //Normalisaton
+
+  const normalizePrediction = (prediction, imgSize) => {
+    if (!prediction || !imgSize || !imageRef) return prediction || [];
+    return prediction.map((prediction) => {
+      const { bbox } = prediction;
+
+      const oldX = bbox[0];
+      const oldY = bbox[1];
+      const oldWidth = bbox[2];
+      const oldHeight = bbox[3];
+
+      const imgWidth = imageRef.current.width;
+      const imgHeight = imageRef.current.height;
+
+      const x = (oldX * imgWidth) / imgSize.width;
+      const y = (oldY * imgHeight) / imgSize.height;
+      const width = (oldWidth * imgWidth) / imgSize.width;
+      const height = (oldHeight * imgHeight) / imgSize.height;
+
+      return { ...prediction, bbox: [x, y, width, height] };
+    });
+  };
+
   // -------------->> Tensorflow part <----------------------
 
-  const detectObjectOnImage = async (imageElement) => {
+  const detectObjectOnImage = async (imageElement, imgSize) => {
     console.log("modellling is running in the back");
     //loading coco model..
     const model = await cocoSsd.load({});
@@ -52,7 +76,8 @@ const ObjectDetector = (props) => {
     //we detect model having the image..
 
     const predictions = await model.detect(imageElement, 6);
-    setPrediction(predictions);
+    const normalizePredictions = normalizePrediction(predictions, imgSize);
+    setPrediction(normalizePredictions);
     console.log("predictions", predictions);
   };
 
@@ -66,6 +91,8 @@ const ObjectDetector = (props) => {
   };
 
   const onSelectImage = async (e) => {
+    setPrediction([]);
+    setIsLoading(true);
     const file = e.target.files[0];
     const imgData = await readImage(file);
     setImgData(imgData);
@@ -74,39 +101,55 @@ const ObjectDetector = (props) => {
     imageElement.src = imgData;
 
     imageElement.onload = async () => {
-      await detectObjectOnImage(imageElement);
+      const imgSize = {
+        width: imageElement.width,
+        height: imageElement.height,
+      };
+      await detectObjectOnImage(imageElement, imgSize);
+      setIsLoading(false);
     };
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.detector}>
-        {imgData && (
-          <img src={imgData} className={styles.target} alt="imgData" />
-        )}
-        {!isEmptyPredications &&
-          prediction.map((predication, indx) => (
-            <TargetBox
-              key={indx}
-              x={predication.bbox[0]}
-              y={predication.bbox[1]}
-              width={predication.bbox[2]}
-              height={predication.bbox[3]}
-              classType={predication.class}
-              score={predication.score}
-            />
-          ))}
-        <input
-          className={styles.hidden}
-          onChange={onSelectImage}
-          type="file"
-          ref={fileInputRef}
-        />
+    <div>
+      <div className={styles.heading}>
+        <span>Image Recognizing AI</span>
       </div>
-      <div>
-        <button className={styles.selectbutton} onClick={openFilePicker}>
-          Select Image
-        </button>
+
+      <div className={styles.container}>
+        <div className={styles.detector}>
+          {imgData && (
+            <img
+              src={imgData}
+              className={styles.target}
+              alt="imgData"
+              ref={imageRef}
+            />
+          )}
+          {!isEmptyPredications &&
+            prediction.map((predication, indx) => (
+              <TargetBox
+                key={indx}
+                x={predication.bbox[0]}
+                y={predication.bbox[1]}
+                width={predication.bbox[2]}
+                height={predication.bbox[3]}
+                classType={predication.class}
+                score={predication.score * 100}
+              />
+            ))}
+          <input
+            className={styles.hidden}
+            onChange={onSelectImage}
+            type="file"
+            ref={fileInputRef}
+          />
+        </div>
+        <div>
+          <button className={styles.selectbutton} onClick={openFilePicker}>
+            {isLoading ? "Recognizing...." : "Select Image"}
+          </button>
+        </div>
       </div>
     </div>
   );
